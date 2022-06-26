7ef48d0cd04a0d4506b34e5291c4456dc0d328e6
위예나 <yaena0319@naver.com>
2022-06-26 16:57:12 +0900

    day2mandatory

--- Day 2/FlexGrid/src/Router1.js
+++ Day 2/FlexGrid/src/Router1.js
@@ -1,10 +1,9 @@
-import React from "react";
-import Flex from "./Flex";
+import React from 'react';
+import Flex from './Flex';
 
 export default function Router1() {
   return (
     <>
-      <h1>Flex</h1>
       <Flex />
     </>
   );
